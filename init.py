15439c19962cc16aae53f631ec19be953a919173
Ara Sadoyan <ara.sadoyan@netangels.net>
2018-10-31 16:46:38 +0400

    Pi3 Web interface

--- init.py
+++ init.py
@@ -7,9 +7,6 @@ def application(environ, start_response):
     length = int(environ.get('CONTENT_LENGTH', '0'))
     body = environ['wsgi.input'].read(length)
     p = lib.parseinput.ParseInput()
-    # if datatype == 'influx':
-    #     r = p.influx(body)
-    # else:
     r = p.oddeye(body)
     resp = str(r)
     start_response(resp + ' OK', [('Content-Type', 'text/plain')])
